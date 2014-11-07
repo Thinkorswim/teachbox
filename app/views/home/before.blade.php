bcffb443200d631ac0badb0483c8df23079f0668
ivanlebanov <ivan.lebanov@abv.bg>
2014-11-07 08:17:21 +0000

    Registation and login design

--- app/views/home/before.blade.php
+++ app/views/home/before.blade.php
@@ -8,49 +8,95 @@
 				{{Session::get('global')}}
 		</div>
 	@endif
-	
-	<a href="{{ URL::route('password-recovery') }}">Forgot Password</a>
-<!-- Sign in -->
-	{{ Form::open(['route' => 'sign-in']) }}
-		 {{ Form::text('email_s', null , array('placeholder'=>'E-mail')) }}
-		  @if($errors->has('email_s'))
-		 	{{$errors->first('email_s')}}
-		  @endif
-		 </br>
-		 {{ Form::password('password_s', array('placeholder'=>'Password')) }}
-		  @if($errors->has('password_s'))
-		 	{{$errors->first('password_s')}}
-		  @endif
-		 </br>
-		 {{ Form::label('remember', 'Remember me') }}
-		 {{ Form::checkbox('remember') }}
-		  </br>
-		 {{ Form::submit('Sign in') }}
-	{{ Form::close() }}
+	<section class="full-screen">
+		<div class="container">
+			<div class="col-xs-1 col-sm-3 col-md-4">
+				<!--<h1>Education is the key!</h1>-->
+			</div>
+			<div class="col-xs-10 col-sm-6 col-md-4 tab-register">	
+				<ul class="nav nav-tabs" role="tablist">
+				  <li role="presentation" class="active"><a href="#login" role="tab" data-toggle="tab">Login</a></li>
+				  <li role="presentation"><a href="#register" role="tab" data-toggle="tab">Register</a></li>
+				</ul>
+				<div class="tab-content">	
+				<!-- Login -->
+				    <div role="tabpanel" class="tab-pane in active" id="login">	
+						<a class="btn btn-lg btn-fb" href="">
+						<i class="fa fa-facebook"></i> Login with Facebook
+						</a>
+						<h6><span  class="line-center">or</span></h6>					
+						<div class="input-group">
+						  <span class="input-group-addon"><i class="pe-7s-mail"></i></span>					
+						{{ Form::open(['route' => 'sign-in']) }}
+							 {{ Form::text('email_s', null , array('placeholder'=>'E-mail','class'=>'form-control')) }}
+							  @if($errors->has('email_s'))
+								{{$errors->first('email_s')}}
+							  @endif
+						</div>
+						<div class="input-group">
+						  <span class="input-group-addon"><i class="pe-7s-lock"></i></span>
+							 {{ Form::password('password_s', array('placeholder'=>'Password','class'=>'form-control')) }}
+							  @if($errors->has('password_s'))
+								{{$errors->first('password_s')}}
+							  @endif
+						</div>
+						<div class="row">
+							<div class="col-xs-6">
+								<div class="checkbox">
+									<label>
+										 {{ Form::checkbox('remember') }} Remember me
+									 </label>
+								</div>
+							</div>
+							<div class="col-xs-6">
+								<a href="{{ URL::route('password-recovery') }}">Forgot Password</a>
+							</div>
+						</div>
+						<div class="input-group submit">
+							 {{ Form::submit('Login', array('class'=>'form-control')) }}
+						</div>
+						{{ Form::close() }}
+					</div>
 
 
-<!-- Sign up -->
-	{{ Form::open(['route' => 'create-account']) }}	 
-		 {{ Form::text('email', null , array('placeholder'=>'E-mail')) }}
-		 @if($errors->has('email'))
-		 	{{$errors->first('email')}}
-		 @endif
-		 </br>
-		 {{ Form::text('name', null,  array('placeholder'=>'Username')) }}
-		 @if($errors->has('name'))
-		 	{{$errors->first('name')}}
-		 @endif
-		  </br>
-		 {{ Form::password('password', array('placeholder'=>'Password')) }}
-		 @if($errors->has('password'))
-		 	{{$errors->first('password')}}
-		 @endif 
-		  </br>
-		 {{ Form::password('password_again', array('placeholder'=>'Repeat Password')) }}
-		 @if($errors->has('password_again'))
-		 	{{$errors->first('password_again')}}
-		 @endif
-		  </br>
-		 {{ Form::submit('Register') }}
-	{{ Form::close() }}
+					<!-- Registration -->
+					<div role="tabpanel" class="tab-pane" id="register">
+						{{ Form::open(['route' => 'create-account']) }}	 
+						<div class="input-group">
+						  <span class="input-group-addon"><i class="pe-7s-user"></i></span>
+							 {{ Form::text('name', null,  array('placeholder'=>'Username', 'class'=>'form-control')) }}
+							 @if($errors->has('name'))
+								{{$errors->first('name')}}
+							 @endif
+						</div>
+						<div class="input-group">
+						  <span class="input-group-addon"><i class="pe-7s-mail"></i></span>
+							 {{ Form::text('email', null , array('placeholder'=>'E-mail', 'class'=>'form-control', 'id'=>'mail')) }}
+							 @if($errors->has('email'))
+								{{$errors->first('email')}}
+							 @endif
+						</div>
+						<div class="input-group">
+						  <span class="input-group-addon"><i class="pe-7s-lock"></i></span>
+							 {{ Form::password('password', array('placeholder'=>'Password', 'class'=>'form-control', 'id'=>'password')) }}
+							 @if($errors->has('password'))
+								{{$errors->first('password')}}
+							 @endif 
+						</div>
+						<div class="input-group">
+						  <span class="input-group-addon"><i class="pe-7s-lock"></i></span>
+							 {{ Form::password('password_again', array('placeholder'=>'Repeat Password', 'class'=>'form-control', 'id'=>'repeat')) }}
+							 @if($errors->has('password_again'))
+								{{$errors->first('password_again')}}
+							 @endif
+						</div>
+						<div class="input-group submit">
+							 {{ Form::submit('Register', array('class'=>'form-control')) }}
+						</div>
+						{{ Form::close() }}
+					</div>
+			</div>
+		</div>
+	</div>
+	</section>
 @endsection
--- app/views/layouts/master-before.blade.php
+++ app/views/layouts/master-before.blade.php
@@ -1,8 +1,42 @@
-<html>
-	<head>
-		<title> Teachbox </title>
-	</head>
-	<body>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Teachbox</title>
+    <link rel="stylesheet" href="{{ URL::asset('css/styles.css') }}" >
+	<link href="{{ URL::asset('css/pe-icon-7-stroke.css" rel="stylesheet') }}" />
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+  	<header>
+		<nav class="navbar navbar-default" role="navigation">
+		  <div class="container">
+			 <a class="centered-brand" href="#"><img src="{{ URL::asset('img/logo.png') }}"/>teachbox</a>
+		  </div><!-- /.container-fluid -->
+		</nav>		
+	</header>
 	    @yield('content')
-	</body>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="{{ URL::asset('js/bootstrap.min.js') }}"></script>
+	<script>
+	$(document).ready(function () {
+		$('#register #password').tooltip({'trigger':'focus', 'title': 'Your password needs to be 6-20 characters','placement' : 'bottom'});
+		$('#register #repeat').tooltip({'trigger':'focus', 'title': 'Repeat the password','placement' : 'bottom'});
+		$('#register #mail').tooltip({'trigger':'focus', 'title': 'It will be used for your authenticaion','placement' : 'bottom'});
+		$('.input-group').click(function(e) {
+		    e.stopPropagation();
+		$('.input-group').removeClass('current');
+		$(this).addClass('current');
+	});
+	$('body').click(function(e) {
+	$('.input-group').removeClass('current');
+		});
+	});
+	</script>
+   </body>
 </html>
