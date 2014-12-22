99e02871c9e3bbfa0c9557bfcf67194fad78aa91
ivanlebanov <ivan.lebanov@abv.bg>
2014-12-22 18:50:53 +0200

    Revert "#3 Crop image by user functionality"
    This reverts commit ab1b371bc50290646bd3b3c6ad89fb6ea73b8eb9.

--- app/views/profile/pic.blade.php
+++ app/views/profile/pic.blade.php
@@ -3,73 +3,17 @@
 @section('content')
 	<a href="{{ URL::action('ProfileController@userSettings', [$user->id]) }}"> Edit Profile </a>
 	<br> <br>
-
-	<a href="#" data-toggle="modal" data-target="#myModal"> Change picture </a>
+	<a href="{{ URL::action('ProfileController@changePic', [$user->id]) }}"> Change picture </a>
 	<br> <br>
 	@if($user->active == 1)
 		<a href="{{ URL::action('ProfileController@changePassword', [$user->id]) }}"> Change password </a>
 	@endif
-	<script src="http://yui.yahooapis.com/3.17.2/build/yui/yui-min.js"></script>
-	<script src="{{ URL::asset('js/cropbox.js') }}"></script>
-<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal"><i class=" fa-2x pe-7s-close"></i></button>
-        <h4 class="modal-title">Change picture</h4>
-      </div>
-      <div class="modal-body">
-        	{{ Form::open(array('action' => array('ProfileController@postChangePic', $user->id), 'files' => true  )) }}
-		<div class="imageBox">
-	        <div class="thumbBox"></div>
-	        <div class="spinner" style="display: none">Loading...</div>
-			<div class="fileUpload btn btn-primary">
-			    <span>Select Picture</span>
-			    {{ Form::file('image', array('placeholder'=>'Password','id'=>'file','class' => 'upload')) }}
-			</div>
-		    <div class="actions">
-		       	<input type="button" id="btnZoomIn" class="btn btn-primary" value="+" style="float: right">
-		        <input type="button" id="btnZoomOut" class="btn btn-primary" value="-" style="float: right">
-		    </div>
-	    </div>
+	
+	{{ Form::open(array('action' => array('ProfileController@postChangePic', $user->id), 'files' => true  )) }}
+		{{ Form::file('image') }}
 
-	    <div class="cropped">
+		{{ Form::submit('upload') }}
 
-	    </div>
-		<script type="text/javascript">
-		    YUI().use('node', 'crop-box', function(Y){
-		        var options =
-		        {
-		            imageBox: '.imageBox',
-		            thumbBox: '.thumbBox',
-		            spinner: '.spinner',
-		            imgSrc: 'avatar.png'
-		        }
-		        var cropper = new Y.cropbox(options);
-		        Y.one('#file').on('change', function(){
-		            var reader = new FileReader();
-		            reader.onload = function(e) {
-		                options.imgSrc = e.target.result;
-		                cropper = new Y.cropbox(options);
-		            }
-		            reader.readAsDataURL(this.get('files')._nodes[0]);
-		            this.get('files')._nodes = [];
-		        })
-		        Y.one('#btnZoomIn').on('click', function(){
-		            cropper.zoomIn();
-		        })
-		        Y.one('#btnZoomOut').on('click', function(){
-		            cropper.zoomOut();
-		        })
-		    })
-		</script>
-      	{{ Form::submit('upload', array('placeholder'=>'Password','class'=>'form-control')) }}
-        	{{ Form::token() }}
+		{{ Form::token() }}
 	{{ Form::close() }}
-      </div>
-    </div>
-  </div>
-</div>
-
-</div>
 @endsection
