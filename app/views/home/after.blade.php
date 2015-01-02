428e7bb6d773fb92bea1019037de22dfe963427b
ivanlebanov <ivan.lebanov@abv.bg>
2015-01-02 23:33:20 +0000

    one more line

--- app/views/home/after.blade.php
+++ app/views/home/after.blade.php
@@ -6,6 +6,7 @@
 	<div class="col-xs-3">
 		<video class="video-js" preload="auto" poster="{{ URL::asset('img/82.jpg') }}" data-setup="{}">
 		  <source src="http://iurevych.github.com/Flat-UI-videos/big_buck_bunny.mp4" type="video/mp4">
+		  	
 		</video>
 	</div>
 </div>
