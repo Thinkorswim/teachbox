a3395d3a98098bc63f9756a9f9b9b07a8fcf09b0
llluchko <l.mladenov21@gmail.com>
2015-02-22 16:52:30 +0200

    ignore config files (adding local environment)

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,8 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('homestead'),
+	'local' => array('WIN-2PL1M7KA7DQ'),
+	'production' => array('http://teachbox.io/'),
 
 ));
 
