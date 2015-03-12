85210fcaa6de4e1fd4da30551badc7c10500cbee
Thinkorswim <thinkorswim1@abv.bg>
2015-03-12 19:52:44 +0000

    ffmpeg v8

--- app/routes.php
+++ app/routes.php
@@ -419,7 +419,8 @@ class Convert {
     	$video = $data["video"];
     	$path = $data["path"];
 
-		$cmd = "$ffmpeg -i $video -vcodec libvpx -cpu-used -5 -deadline realtime $path/video.webm";
+		//$cmd = "$ffmpeg -i $video -vcodec libvpx -cpu-used -5 -deadline realtime $path/video.webm";
+		$cmd = "$ffmpeg -i $video -acodec libvorbis -aq 5 -ac 2 -qmax 25 -threads 2 $path/video.webm"
 		shell_exec($cmd);
 
         $job->delete();
